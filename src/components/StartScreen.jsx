21771352ee6fcbc1b5bdb81188cfc434556ea6e2
7961-Ephy <ephykamau19@gmail.com>
2024-12-10 04:06:41 +0300

    first commit - useReducer hook react

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+function StartScreen({ numQuestions, dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz</h2>
+      <h3>{numQuestions} questions to start your React mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let&apos;s Start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
